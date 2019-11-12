cafda54dfa9ae2dcb020b0b0ca7d27beb9d22b14
Ricardo Persaud <ricardo39985@gmail.com>
2019-11-12 01:53:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,7 +38,7 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  if false in source_array
+  if source_array.include?(false)
     return false
   else
     return true
